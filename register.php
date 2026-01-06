3bb42d3fbe293c323a3bb12106ae0bd9d0fd0469
TheSzlamek1 <130081528+TheSzlamek1@users.noreply.github.com>
2026-01-06 14:39:40 +0100

    register.php

--- register.php
+++ register.php
@@ -2,16 +2,22 @@
 if($_POST){
 $users = json_decode(file_get_contents('data/users.json'), true);
 $users[] = [
-'username'=>$_POST['username'],
-'password'=>password_hash($_POST['password'], PASSWORD_DEFAULT),
-'role'=>'user'
+'login'=>$_POST['login'],
+'pass'=>password_hash($_POST['pass'], PASSWORD_DEFAULT),
+'role'=>$_POST['role'],
+'balance'=>0
 ];
 file_put_contents('data/users.json', json_encode($users));
 header('Location: login.php');
 }
 ?>
+<style>body{background:#111;color:#eee}</style>
 <form method="post">
-<input name="username" placeholder="login">
-<input name="password" type="password" placeholder="hasło">
+<input name="login" placeholder="login"><br>
+<input name="pass" type="password" placeholder="hasło"><br>
+<select name="role">
+<option value="buyer">Kupujący</option>
+<option value="seller">Sprzedawca</option>
+</select><br>
 <button>Rejestruj</button>
 </form>
